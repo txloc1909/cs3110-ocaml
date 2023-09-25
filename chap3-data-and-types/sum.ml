8b5e7bb5e9779ea04376c07ec2d34cc74da4d3c5
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-09-25 22:02:43 +0700

    deliberately introduce a bug, and improve ounit output

--- chap3-data-and-types/sum.ml
+++ chap3-data-and-types/sum.ml
@@ -1,3 +1,3 @@
 let rec sum = function
-    | [] -> 0
+    | [] -> 1
     | x :: xs -> x + sum xs
--- chap3-data-and-types/test.ml
+++ chap3-data-and-types/test.ml
@@ -2,9 +2,9 @@ open OUnit2
 open Sum
 
 let tests = "test suite for sum" >::: [
-    "empty"         >:: (fun _ -> assert_equal 0 (sum []));
-    "singleton"     >:: (fun _ -> assert_equal 1 (sum [1]));
-    "two_elements"  >:: (fun _ -> assert_equal 3 (sum [1; 2]));
+    "empty"         >:: (fun _ -> assert_equal 0 (sum [])     ~printer:string_of_int);
+    "singleton"     >:: (fun _ -> assert_equal 1 (sum [1])    ~printer:string_of_int);
+    "two_elements"  >:: (fun _ -> assert_equal 3 (sum [1; 2]) ~printer:string_of_int);
 ]
 
 let _ = run_test_tt_main tests
