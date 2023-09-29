b363e934e16573f0f3c667323f27d70c8f049e29
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-09-29 14:17:54 +0700

    refactor next_weekday tests

--- chap3-data-and-types/test.ml
+++ chap3-data-and-types/test.ml
@@ -5,6 +5,9 @@ open Weekday
 let make_sum_test name expected_output input =
     name >:: (fun _ -> assert_equal expected_output (sum input) ~printer:string_of_int)
 
+let make_weekday_test name expected_output input =
+    name >:: (fun _ -> assert_equal expected_output (next_weekday input))
+
 let sum_tests = "test suite for sum" >::: [
     make_sum_test "empty"        0 [];
     make_sum_test "singleton"    1 [1];
@@ -12,13 +15,13 @@ let sum_tests = "test suite for sum" >::: [
 ]
 
 let weekday_tests = "test suite for next_weekday" >::: [
-    "tue_after_mon" >:: (fun _ -> assert_equal Tuesday (next_weekday Monday));
-    "wed_after_tue" >:: (fun _ -> assert_equal Wednesday (next_weekday Tuesday));
-    "thu_after_wed" >:: (fun _ -> assert_equal Thursday (next_weekday Wednesday));
-    "fri_after_thu" >:: (fun _ -> assert_equal Friday (next_weekday Thursday));
-    "sat_after_fri" >:: (fun _ -> assert_equal Saturday (next_weekday Friday));
-    "sun_after_sat" >:: (fun _ -> assert_equal Sunday (next_weekday Saturday));
-    "mon_after_sun" >:: (fun _ -> assert_equal Monday (next_weekday Sunday));
+    make_weekday_test "tue_after_mon" Tuesday Monday;
+    make_weekday_test "wed_after_tue" Wednesday Tuesday;
+    make_weekday_test "thu_after_wed" Thursday Wednesday;
+    make_weekday_test "fri_after_thu" Friday Thursday;
+    make_weekday_test "sat_after_fri" Saturday Friday;
+    make_weekday_test "sun_after_sat" Sunday Saturday;
+    make_weekday_test "mon_after_sun" Monday Sunday;
 ]
 
 let _ = run_test_tt_main sum_tests
